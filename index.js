b59b0a510003cc30b18a20074e8ca062fd764271
sandiarbaa <sandiarbaa@gmail.com>
2024-04-19 10:47:31 +0700

    docs: test several methods of sending and receiving data

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
 
 // route utama / endpoint menggunakan method GET
 app.get("/", (request, response) => {
@@ -8,14 +11,27 @@ app.get("/", (request, response) => {
 });
 
 app.get("/hello", (req, res) => {
-  res.send("Hello World!");
+  // console.log({ urlParam: req.query }); // ini kalau mau semua datanya
+  console.log({ urlParam: req.query.name }); // ini kalau mau datanya spesifik
+  res.send("Hello World! Hello Dunia!");
 });
 
 // kalau menggunakan method POST
-app.get("/login", (req, res) => {
-  if (req.name === "Sandi") {
-    res.send("Login Succes!");
+app.post("/login", (req, res) => {
+  console.log({ requestFromExternalResource: req.body }); // req ini sesuatu yg di dapat dari luar, ex: dari postman
+
+  // jadi ini fungsinya jika kita ingin mengambil datanya spesifik, misal ini contoh ketika ingin validasi
+  const username = req.body.username; // ambil user yg dikirim dari api
+  if (username === usernameFromDB) {
+    // kalau usernamenya ada di database, maka dia boleh login
+    res.status(200).send("Login Success!");
   }
+  res.send("Login Succes!"); // ini mengirim ke luar sesuatu yg ada di sistem, di sini mengirim string
+});
+
+app.put("/username", (req, res) => {
+  console.log({ updateData: req.body });
+  res.send("Update Succes!");
 });
 
 // untuk menjalankan port
